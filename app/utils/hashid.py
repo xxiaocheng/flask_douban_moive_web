f8941bf5ee87c58c811d2da44b737057437be458
ChengXiao <cxxlxx0@gmail.com>
2020-02-14 04:43:40 +0800

    add api_v2 movie api

--- app/utils/hashid.py
+++ app/utils/hashid.py
@@ -1,14 +1,14 @@
 from flask import current_app
 from hashids import Hashids
 
-hashids = Hashids(salt=current_app.config["HASHIDS_SALT"], min_length=16)
-
 
 def encode_id_to_str(id):
+    hashids = Hashids(salt=current_app.config["HASHIDS_SALT"], min_length=16)
     return hashids.encode(id)
 
 
 def decode_str_to_id(str):
+    hashids = Hashids(salt=current_app.config["HASHIDS_SALT"], min_length=16)
     try:
         return hashids.decode(str)[0]
     except IndexError:
--- app/v2/__init__.py
+++ app/v2/__init__.py
@@ -1,17 +1,31 @@
 from flask import Blueprint
 from flask_restful import Api
+
+from app.v2.movie import (
+    ChoiceMovie,
+    CinemaMovie,
+    LeaderBoard,
+    Movie,
+    MovieGenresRank,
+    MovieRecommend,
+    MovieUserRating,
+    UserMovie,
+    FollowFeed,
+    Movies,
+)
 from app.v2.user import (
     AuthToken,
-    Users,
-    User,
-    Follow,
-    UserRole,
-    Roles,
+    EmailToken,
     ExistTest,
+    Follow,
+    Roles,
+    User,
     UserEmail,
     UserPassword,
-    EmailToken,
+    UserRole,
+    Users,
 )
+
 
 api_bp = Blueprint("api", __name__, url_prefix="/api/v1")
 
@@ -24,6 +38,25 @@ api.add_resource(Follow, "/users/<username>/<follower_or_following>", endpoint="
 api.add_resource(UserRole, "/users/<username>/role", endpoint="UserRole")
 api.add_resource(Roles, "/roles", endpoint="Roles")
 api.add_resource(ExistTest, "/user/test/<username_or_email>", endpoint="ExistTest")
-api.add_resource(UserEmail, "/user/email")
-api.add_resource(UserPassword, "/user/password")
-api.add_resource(EmailToken, "/user/email/token/<operation>")
+api.add_resource(UserEmail, "/user/email", endpoint="UserEmail")
+api.add_resource(UserPassword, "/user/password", endpoint="UserPassword")
+api.add_resource(EmailToken, "/user/email/token/<operation>", endpoint="EmailToken")
+
+api.add_resource(
+    CinemaMovie, "/movie/cinema/<coming_or_showing>", endpoint="CinemaMovie"
+)
+api.add_resource(MovieRecommend, "/movie/recommend", endpoint="MovieRecommend")
+api.add_resource(
+    LeaderBoard, "/movie/leader-board/<time_range>", endpoint="LeaderBoard"
+)
+api.add_resource(
+    MovieGenresRank, "/movie/genre/<genre_hash_id>", endpoint="MovieGenresRank"
+)
+api.add_resource(UserMovie, "/users/<username>/movie", endpoint="UserMovie")
+api.add_resource(ChoiceMovie, "/movie/choice", endpoint="ChoiceMovie")
+api.add_resource(Movie, "/movie/<movie_hash_id>", endpoint="Movie")
+api.add_resource(
+    MovieUserRating, "/movie/<movie_hash_id>/rating", endpoint="MovieUserRating"
+)
+api.add_resource(FollowFeed, "/movie/feed", endpoint="FollowFeed")
+api.add_resource(Movies, "/movie", endpoint="Movies")
